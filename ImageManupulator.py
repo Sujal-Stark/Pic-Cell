abca5c4194930c041f81e03973eb6733f4d627f0
Sujal-Stark <sujalstark2010@gmail.com>
2024-08-13 22:20:26 +0530

    unsharp mask filter added

--- ImageManupulator.py
+++ ImageManupulator.py
@@ -647,6 +647,38 @@ class FilterImage:
         else:
             return "Succeed"
 
+    # unsharp mask
+    def imageUnsharpMask(self):
+        # copying the image
+        image = self.image
+
+        # create user mask
+        self.user_message = "Enhance details and increase contrast in photos by making objects more identifiable\nRadius Range:(0-10), Threshold Range:(0-10)"
+        self.getMessage()
+
+        # user Choice 
+        print("Radius Range:")
+        radius_choice = self._makeChoice(list(range(0,11)))
+
+        print("Threshold Range:")
+        threshold_choice = self._makeChoice(list(range(0,11)))
+
+        #process
+        try:
+            image =image.convert('RGB').filter(ImageFilter.UnsharpMask(radius=radius_choice, threshold = threshold_choice))
+            self.image = image
+        # excetion handleing
+        except IOError as ioError:
+            return f"Can't write the image file -> {ioError}"
+        except MemoryError as memoryError:
+            return f"Can't load image file in memory -> {memoryError}"
+        except NotImplementedError as notImplementedError:
+            return f"Can't Implement the effect -> {notImplementedError}"
+        except ValueError as valueError:
+            return f"Undefined value -> {valueError}"
+        else:
+            return "Succeed"
+    
     pass
 
 if __name__ == '__main__':
@@ -658,7 +690,7 @@ if __name__ == '__main__':
     filterImage = FilterImage(R"C:\Users\SUJAL KHAN\Downloads\Avengers.png")
 
     #command List
-    print("Command List:\n0--->Save the image\n1--->Open an Image\n2--->Close Image\n3--->Crop Image\n4--->Resize image\n5--->Resample Image\n6--->Rotate an image\n7--->Horizontal Flip\n8--->Vertical Flip\n9---> set Auto Contrast\n10--->GrayScale\n11--->Postarise\n12--->Gaussian BLur\n13--->Sharp image\n14--->Contour\n15---> Add detail\n16--->Smooth out\n17--->Emboss image\n18--->Edge Enhance\n19--->Box Blur\n-1--->To stop programme")
+    print("Command List:\n0--->Save the image\n1--->Open an Image\n2--->Close Image\n3--->Crop Image\n4--->Resize image\n5--->Resample Image\n6--->Rotate an image\n7--->Horizontal Flip\n8--->Vertical Flip\n9---> set Auto Contrast\n10--->GrayScale\n11--->Postarise\n12--->Gaussian BLur\n13--->Sharp image\n14--->Contour\n15---> Add detail\n16--->Smooth out\n17--->Emboss image\n18--->Edge Enhance\n19--->Box Blur\n20--->Unsharp Mask\n-1--->To stop programme")
 
     command = int(input("Enter command:\t")) # takes user command
 
@@ -906,6 +938,18 @@ if __name__ == '__main__':
             
             # main effect
             print(filterImage.boxBlurImage())
+
+            #reassign the universal image object
+            universal_image = filterImage.provideImageObject()
+
+        # Unsharp  Mask
+        elif command ==  20:
+            # setting the instance variable
+            if universal_image != None:
+                filterImage.getImageObject(universal_image)
+            
+            # main effect
+            print(filterImage.imageUnsharpMask())
 
             #reassign the universal image object
             universal_image = filterImage.provideImageObject()
