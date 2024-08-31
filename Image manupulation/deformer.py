ad687861ec55b2320fbdf6439e1d7da47077be88
Sujal-Stark <sujalstark2010@gmail.com>
2024-08-31 18:23:22 +0530

    first two defrom methods are added

--- Image manupulation/deformer.py
+++ Image manupulation/deformer.py
@@ -21,4 +21,46 @@ class ImageDeformer:
     def provideImageObject(self) -> Image.Image:
         return self.image
     
+    # looks like iamge is fast forwarded
+    def middleTwist(self) -> str:
+        image = self.image # copying original image object
+        try:
+            # mesh class will parse to deform method
+            class DeformMesh:
+                def getmesh(self, image):
+                    width , height = image.size
+                    desiredMesh1 = (0,0, 0,height, width, 0, width, height)
+                    targetMesh1 = (0, 0, width, height)
+                    return [(targetMesh1, desiredMesh1)]
+            image = ImageOps.deform(image, DeformMesh())
+        except IOError as ioError:
+            return f"{ioError}"
+        except MemoryError as memoryError:
+            return f"Insufficient Error {memoryError}"
+        except ValueError as valueError:
+            return f"invalid value {valueError}"
+        self.image = image
+        return "Succeed"
+    
+    # twist at both quarter half
+    def doubleTwisted(self) -> str:
+        image = self.image # copying image object
+        try:
+            class DeformMesh:
+                def getmesh(self, image : Image.Image):
+                    width , height = image.size
+                    desiredMesh1 = (0, 0, 0, height, width/2,0, width/2, height)
+                    targetMesh1 = (0, 0, width//2, height)
+                    desiredMesh2 = (width/2, height, width/2, 0, width, height, width, 0)
+                    targetMesh2 = (width//2, 0, width, height)
+                    return [(targetMesh1, desiredMesh1), (targetMesh2, desiredMesh2)]
+            image = ImageOps.deform(image=image, deformer= DeformMesh())
+        except IOError as ioError:
+            return f"{ioError}"
+        except MemoryError as memoryError:
+            return f"Insufficient Error {memoryError}"
+        except ValueError as valueError:
+            return f"invalid value {valueError}"
+        self.image = image
+        return "Succeed"
     pass
