436c637e91e599cf2de99e3e01c6f78fc206ded5
Sujal-Stark <sujalstark2010@gmail.com>
2024-12-29 19:08:52 +0530

    modified to handle both Pixmap and Output PIL image

--- Graphical Interface/pixmapLinker.py
+++ Graphical Interface/pixmapLinker.py
@@ -3,55 +3,29 @@ from PIL import Image
 class Node:
     def __init__(self) -> None:
         self.image : QPixmap # stores teh image in Pixmap Format
+        self.PILImage : Image.Image # stores the image in PIL format
         self.nextNode = None  # store the next node address
         self.previousNode = None  # store the previous node address
         return
-
-# PIL image Node CLass
-class PILNode:
-    def __init__(self) -> None:
-        self.PILimage : Image.Image = None # stores the PIL image
-        self.nextNode : PILNode = None # store the next node address
-        self.previousNode : PILNode = None # store the previous node address
-        return
     
 class PixmapLinker:
-    # varibale heads to store objects 
-    pixmapHead : Node = None # stores pixmap object
-    PILHead  = None # stores PIL object
-
     def __init__(self):
-        # self.pixmapHead : Node = None
+        self.pixmapHead : Node = None # stores pixmap object
         return
 
-    def createPixmappixmapHead(self, img : QPixmap):
+    def createPixmappixmapHead(self, img : QPixmap, PILImage : Image.Image):
         if self.pixmapHead == None:
             self.pixmapHead = Node()
             self.pixmapHead.image = img
+            self.pixmapHead.PILImage = PILImage
             return
     
-    # create a head for PIL image
-    def createPILHead(self, image : Image.Image) -> None:
-        if self.PILHead == None:
-            self.PILHead = PILNode()
-            self.PILHead.PILimage = image
-        return
-    
     # adds other pixmap nodes
-    def addPixmap(self, targetNode : Node, img : QPixmap) -> Node:
+    def addPixmap(self, targetNode : Node, img : QPixmap, PILImage : Image.Image = None) -> Node:
         if (targetNode):
             newNode = Node() # create nodes
             newNode.image = img
-            newNode.nextNode = targetNode # newnode -> targetnode
-            targetNode.previousNode = newNode # newnode <- targetnode
-            targetNode = newNode # newnode = targetnode
-            return targetNode
-    
-    # adds other pil nodes
-    def addPILObject(self, targetNode : PILNode, image : Image.Image) -> PILNode:
-        if(targetNode):
-            newNode = PILNode()
-            newNode.PILimage = image
+            newNode.PILImage = PILImage
             newNode.nextNode = targetNode # newnode -> targetnode
             targetNode.previousNode = newNode # newnode <- targetnode
             targetNode = newNode # newnode = targetnode
