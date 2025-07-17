e88b86e2996b850d5eb3578421cb044879e58a80
Sujal-Stark <sujalstark2010@gmail.com>
2025-07-17 23:07:21 +0530

    constants for edit manager Ui is added

--- Constants.py
+++ Constants.py
@@ -6,6 +6,10 @@ SPLASH_SCREEN_PATH = "./static/Splash_Screen_PicCell.png"
 
 
 
+########################################## MASTER GRAPHICAL INTERFACE ####################################
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+
 ########################################## GALLERY VIEW ##################################################
 GALLERY_VIEW_PANEL_PERCENTAGE = 80
 
@@ -14,3 +18,18 @@ GRID_PANEL_HEIGHT_PERCENTAGE = 0.6
 GRID_VIEW_LABEL_HEIGHT = 40
 INF_PANEL_WIDTH_PERCENTAGE = 20
 INFO_PANEL_HEIGHT_PERCENTAGE = 40
+
+############################################## EDIT MANAGER ###############################################
+EDIT_OPTION_ADJUST = "Adjust"
+EDIT_OPTION_FILTERS = "Filters"
+EDIT_OPTION_COLOR_ENHANCE = "Color Enhance"
+EDIT_OPTION_DEFORM_IMAGE = "Deform Image"
+EDIT_OPTION_FRAMES = "Frames"
+EDIT_OPTION_TEXT_EDITOR = "Text Editor"
+
+EDIT_BODY_WIDTH_PERCENTAGE = 0.2
+EDIT_SECTION_VIEW_PANEL_WIDTH_PERCENTAGE = 0.6
+EDIT_SPECTRUM_WIDTH_PERCENTAGE = 0.2
+EDIT_SPECTRUM_HEIGHT_PERCENTAGE = 0.4
+ADVANCEMENT_PANEL_WIDTH_PERCENTAGE = 0.2
+ADVANCEMENT_PANEL_HEIGHT_PERCENTAGE = 0.3
