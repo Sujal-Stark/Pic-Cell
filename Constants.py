abcb66471a16beeab48ce59e06ea074ce548f2f0
Sujal-Stark <sujalstark2010@gmail.com>
2025-07-17 16:07:12 +0530

    New Constants related to size occupancy in Gallery view is added

--- Constants.py
+++ Constants.py
@@ -1,5 +1,16 @@
 ###################################### Application Related Constants ##########################################
 APPLICATION_NAME = "Pic-Cell"
-SPLASH_SCREEN_TIME = 5000
-ICON_PATH = "./static/Icon_Pic_Cell.png"
+SPLASH_SCREEN_TIME = 2000
+ICON_PATH = "./static/Icon_Pic_Cell.ico"
 SPLASH_SCREEN_PATH = "./static/Splash_Screen_PicCell.png"
+
+
+
+########################################## GALLERY VIEW ##################################################
+GALLERY_VIEW_PANEL_PERCENTAGE = 80
+
+GRID_PANEL_WIDTH_PERCENTAGE = 0.2
+GRID_PANEL_HEIGHT_PERCENTAGE = 0.6
+GRID_VIEW_LABEL_HEIGHT = 40
+INF_PANEL_WIDTH_PERCENTAGE = 20
+INFO_PANEL_HEIGHT_PERCENTAGE = 40
