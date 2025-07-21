092cdb78d34e71687f19b1143f9ee1f006f9ba82
Sujal-Stark <sujalstark2010@gmail.com>
2025-07-21 22:02:40 +0530

    new Constants are added to use File Window properly

--- Constants.py
+++ Constants.py
@@ -1,14 +1,20 @@
 ###################################### Application Related Constants ##########################################
+
 APPLICATION_NAME = "Pic-Cell"
 SPLASH_SCREEN_TIME = 2000
 ICON_PATH = "Resources/static/Icon_Pic_Cell.ico"
 SPLASH_SCREEN_PATH = "Resources/static/Splash_Screen_PicCell.png"
 
-
+########################################### COMMON ####################################################
+IMAGE_OPENED_MESSAGE = "Opened Successfully"
 
 ########################################## MASTER GRAPHICAL INTERFACE ####################################
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 600
+
+########################################## FILe WINDOW ##################################################
+EXTENSION_LIST = "Images (*.png *.jpeg *.jfif *.jpg  *.gif *.bmp *.ppm *.tiff)"
+FILE_WINDOW_TITLE = "Select An Image"
 
 ########################################## GALLERY VIEW ##################################################
 GALLERY_VIEW_PANEL_PERCENTAGE = 80
@@ -36,5 +42,10 @@ ADVANCEMENT_PANEL_WIDTH_PERCENTAGE = 0.2
 ADVANCEMENT_PANEL_HEIGHT_PERCENTAGE = 0.3
 EDIT_MANAGER_UI_STYLE_FILE = "Resources/Style/editManager.qss"
 
+EDITED_TAG = "_EDITED_"
+
 ######################################## CUSTOM CROP WINDOW #################################################
 CUSTOM_CROP_WINDOW_UI_STYLE_FILE = "Resources/Style/customCropWindow.qss"
+
+########################################## FONTS #######################################################
+FONT_COMIC_SANS_MS = "Comic Sans MS"
